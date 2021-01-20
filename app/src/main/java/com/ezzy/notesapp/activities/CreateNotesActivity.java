58061556ccaf96bfb649860345c567c0b3ed9fc1
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-01-21 02:31:44 +0300

    :sparkless:save note into database

--- app/src/main/java/com/ezzy/notesapp/activities/CreateNotesActivity.java
+++ app/src/main/java/com/ezzy/notesapp/activities/CreateNotesActivity.java
@@ -2,13 +2,28 @@ package com.ezzy.notesapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ezzy.notesapp.R;
+import com.ezzy.notesapp.database.NoteDatabase;
+import com.ezzy.notesapp.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNotesActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,65 @@ public class CreateNotesActivity extends AppCompatActivity {
 
         ImageView imageBackButton = findViewById(R.id.imageBack);
         imageBackButton.setOnClickListener(v -> onBackPressed());
+        
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+        textDateTime = findViewById(R.id.textDateTime);
+
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                .format(new Date())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote(){
+        if (isTextEmpty(inputNoteTitle)) {
+            makeToast(getString(R.string.note_title_error));
+        } else if (isTextEmpty(inputNoteSubtitle) && isTextEmpty(inputNoteText)){
+            makeToast(getString(R.string.note_text_err));
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        //doing operations on the background to avoid app freeze
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NoteDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+        new SaveNoteTask().execute();
+    }
+
+    private boolean isTextEmpty(EditText editText) {
+        return editText.getText().toString().trim().isEmpty();
+    }
+
+    private void makeToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/ezzy/notesapp/activities/MainActivity.java
+++ app/src/main/java/com/ezzy/notesapp/activities/MainActivity.java
@@ -3,12 +3,17 @@ package com.ezzy.notesapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.ezzy.notesapp.R;
 import com.ezzy.notesapp.activities.CreateNotesActivity;
+import com.ezzy.notesapp.database.NoteDatabase;
+import com.ezzy.notesapp.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getNotes();
 
         ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
         imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +36,24 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+    }
+
+    private void getNotes() {
+
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDatabase.getDatabase(getApplicationContext())
+                        .noteDao()
+                        .getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+            }
+        }
+
+        new GetNoteTask().execute();
     }
 }
